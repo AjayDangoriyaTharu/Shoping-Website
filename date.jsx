6b04796b198c1fe1758ddb6f5296ad417bfa653e
creesdangoriya07 <dangoriyacrees@gmail.com>
2023-06-28 23:32:13 +0530

    06/28

---
+++ date.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    const currYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright @ currYear</p>
+        </footer>
+    );
+}
+export default Footer
